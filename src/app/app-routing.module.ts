51cbad9e4ac2ce0ac0aab11bdcb81f4011ce216f
Tanapol Chamnanhan <tawitty.tc@gmail.com>
2023-02-12 19:23:08 +0700

    feat: create about-us and profile component
    feat: create about-us and profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { PagesComponent } from './pages/pages.component';
 import { ProductsComponent } from './products/products.component';
 import { SettingsComponent } from './settings/settings.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { DevicesComponent } from './devices/devices.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
@@ -22,7 +25,10 @@ const routes: Routes = [
   },
   {path: 'pages', component: PagesComponent},
   {path: 'media', component: MediaComponent},
-  {path: 'settings', component: SettingsComponent}
+  {path: 'settings', component: SettingsComponent},
+  {path: 'devices', component: DevicesComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -6,46 +6,47 @@ export const navbarData: INavbarData[] = [
         icon: 'fal fa-home',
         label: 'Dashboard'
     },
+ 
+    // {
+    //     routeLink: 'products',
+    //     icon: 'fal fa-plug',
+    //     label: 'Devices',
+    //     items: [
+    //         {
+    //             routeLink: 'products/level1.1',
+    //             label: 'Songkhla',
+    //             items: [
+    //                 {
+    //                     routeLink: 'products/level2.1',
+    //                     label: 'PSU',
+    //                 },
+    //                 {
+    //                     routeLink: 'products/level2.2',
+    //                     label: 'RUTS',
+    //                     items: [
+    //                         {
+    //                             routeLink: 'products/level3.1',
+    //                             label: 'Building 62'
+    //                         },
+    //                         {
+    //                             routeLink: 'products/level3.2',
+    //                             label: 'Building 50'
+    //                         }
+    //                     ]
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             routeLink: 'products/level1.2',
+    //             label: 'Bangkok',
+    //         }
+    //     ]
+    // },
     {
         routeLink: 'devices',
         icon: 'fal fa-plug',
-        label: 'Devices',
-        items: [
-            {
-                routeLink: 'devices/level1.1',
-                label: 'Songkhla',
-                items: [
-                    {
-                        routeLink: 'products/level2.1',
-                        label: 'PSU',
-                    },
-                    {
-                        routeLink: 'products/level2.2',
-                        label: 'RUTS',
-                        items: [
-                            {
-                                routeLink: 'products/level3.1',
-                                label: 'Building 62'
-                            },
-                            {
-                                routeLink: 'products/level3.2',
-                                label: 'Building 50'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                routeLink: 'products/level1.2',
-                label: 'Bangkok',
-            }
-        ]
+        label: 'Devices'
     },
-    // {
-    //     routeLink: 'statistics',
-    //     icon: 'fal fa-chart-bar',
-    //     label: 'Statistics'
-    // },
     // {
     //     routeLink: 'coupens',
     //     icon: 'fal fa-tags',
@@ -62,7 +63,7 @@ export const navbarData: INavbarData[] = [
     //     ]
     // },
     {
-        routeLink: 'pages',
+        routeLink: 'about-us',
         icon: 'fal fa-handshake',
         label: 'About us'
     },
@@ -73,7 +74,7 @@ export const navbarData: INavbarData[] = [
     //     label: 'About us'
     // },
     {
-        routeLink: 'settings',
+        routeLink: 'profile',
         icon: 'fal fa-user',
         label: 'Profile',
         // expanded: true,
@@ -89,6 +90,7 @@ export const navbarData: INavbarData[] = [
         //     }
         // ]
     },
+    
     {
         routeLink: 'logout',
         icon: 'fal  fa-sign-out',
